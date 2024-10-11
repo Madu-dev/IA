ccd664843a59f15816e9d974ec09cdebfb8e37c8
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2024-10-11 19:04:25 +0000

    add loop

--- resolucao/Main.java
+++ resolucao/Main.java
@@ -14,24 +14,32 @@ public class Main {
                 {7, 8, 0}
         };
 
-        PrimeiraImplementacao primeira = new PrimeiraImplementacao(raiz, objetivo);
-        System.out.println("Iniciando a Primeira Implementação...");
-        primeira.gerarAmostrasCopia(primeira.getRaiz(), 10, 0);
+        int limiteProfundidadeMaximo = 10;
 
-        if (primeira.isSolucaoEncontrada()) {
-            System.out.println("Solução encontrada na Primeira Implementação!");
-        } else {
-            System.out.println("Solução não encontrada na Primeira Implementação.");
+        for (int limiteProfundidade = 1; limiteProfundidade <= limiteProfundidadeMaximo; limiteProfundidade++) {
+            PrimeiraImplementacao primeira = new PrimeiraImplementacao(raiz, objetivo);
+            System.out.println("Iniciando a Primeira Implementação com limite de profundidade: " + limiteProfundidade);
+            primeira.gerarAmostrasCopia(primeira.getRaiz(), limiteProfundidade, 0);
+
+            if (primeira.isSolucaoEncontrada()) {
+                System.out.println("Solução encontrada na Primeira Implementação com limite de profundidade: " + limiteProfundidade);
+                break;
+            } else {
+                System.out.println("Solução não encontrada na Primeira Implementação com limite de profundidade: " + limiteProfundidade);
+            }
         }
 
-        SegundaImplementacao segunda = new SegundaImplementacao(raiz, objetivo);
-        System.out.println("Iniciando a Segunda Implementação...");
-        segunda.gerarAmostrasDireto(segunda.getRaiz(), 10, 0);
+        for (int limiteProfundidade = 1; limiteProfundidade <= limiteProfundidadeMaximo; limiteProfundidade++) {
+            SegundaImplementacao segunda = new SegundaImplementacao(raiz, objetivo);
+            System.out.println("Iniciando a Segunda Implementação com limite de profundidade: " + limiteProfundidade);
+            segunda.gerarAmostrasDireto(segunda.getRaiz(), limiteProfundidade, 0);
 
-        if (segunda.isSolucaoEncontrada()) {
-            System.out.println("Solução encontrada na Segunda Implementação!");
-        } else {
-            System.out.println("Solução não encontrada na Segunda Implementação.");
+            if (segunda.isSolucaoEncontrada()) {
+                System.out.println("Solução encontrada na Segunda Implementação com limite de profundidade: " + limiteProfundidade);
+                break;
+            } else {
+                System.out.println("Solução não encontrada na Segunda Implementação com limite de profundidade: " + limiteProfundidade);
+            }
         }
     }
 }
